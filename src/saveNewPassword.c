eb21e5d916fa263539101f6519081cce7d58b0be
Qu0i_ <quoiteam.com@gmail.com>
2025-02-28 22:24:35 +0300

    --

--- src/saveNewPassword.c
+++ src/saveNewPassword.c
@@ -4,7 +4,6 @@
 int saveNewPassword() {
   FILE *file = fopen("../data/passwords.txt", "a");
 
-  // Error output when a file cannot be opened
   if (file == NULL) {
     printf("Error opening file!\n");
     return 1;
@@ -12,7 +11,6 @@ int saveNewPassword() {
 
   char site[30], login[30], password[50];
 
-  // Entering user data
   printf("Enter site: ");
   scanf("%s", site);
   printf("Enter your login: ");
@@ -20,13 +18,11 @@ int saveNewPassword() {
   printf("Enter your password: ");
   scanf("%s", password);
 
-  // Writing data to a file
   fprintf(file, "site: %s\n", site);
   fprintf(file, "login: %s\n", login);
   fprintf(file, "password: %s\n", password);
   fprintf(file, "-------------------------\n");
 
-  // Closing a file
   fclose(file);
   printf("Data saved successfully\n");
 
